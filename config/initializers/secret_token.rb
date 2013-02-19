c5a060febbbee9ed906b328bc1c2b5b72e325ae0
flammi <flammi88@gmail.com>
2013-02-19 16:22:07 +0100

    Initiale Version des Ecotunes Projektes mit leerem Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecotunes::Application.config.secret_token = 'e5f6dee857d2ea38d5c7441b241a105388814a37508966e9df84880481e220e93715492c5629b9560a0f6cd95f9b0891ff3e6c526f7499df992732a276279488'
