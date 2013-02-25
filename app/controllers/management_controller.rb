f57c994e4e290b34b4e2611561a9f4e9798de291
Fabian Franzen <flammi@watteimdocht.de>
2013-02-25 15:55:04 +0100

    Duplikatcheck fertig

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -2,5 +2,13 @@ class ManagementController < ApplicationController
   def management
   end
   def duplicatecheck
+    songs = Song.all
+    songs_without_dups = songs.uniq do |song| song.finger_print end
+
+    songs_to_delete = songs - songs_without_dups
+    @duplicate_count = songs_to_delete.count
+    songs_to_delete.each { |song|
+      song.destroy
+    }
   end
 end
