5cbb717c6ec0b884155a0988f3e8a83b75d0fecf
Daniel Langerenken <daniel@tzi.de>
2013-02-26 02:13:05 +0100

    Slide in and out implemented

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,4 +23,19 @@ jQuery(function() {
     }).blur(function() {
         jQuery(this).animate({ width: '100px'});
     });
+
+    jQuery('#close-open').click(function() {
+      if ($('#close-open').hasClass("is-open")){
+        $('#close-open').removeClass("is-open").addClass("is-closed");
+        $('#close-open').text(" Open ");
+        $('#close-open').append('<i class="icon-chevron-up"></i>');
+
+        $('#player').slideUp("slow");
+      }else{
+        $('#close-open').removeClass("is-closed").addClass("is-open");
+        $('#close-open').text(" Close ");
+        $('#close-open').append('<i class="icon-chevron-down"></i>');
+        $('#player').slideDown("slow");
+      }
+    });
 });
