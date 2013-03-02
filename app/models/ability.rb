632bf938c195fcfb1a2909033230c6f850850e89
Daniel Langerenken <daniel@tzi.de>
2013-03-02 18:19:25 +0100

    Tests der SOngs und User - WORKS :))

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,7 @@ class Ability
       can :manage, :all
     elsif user.has_role? :user
       can :read, :all
+      cannot :read, User
     end
 
     
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,9 +2,21 @@ require 'spec_helper'
 
 describe UsersController do
 
-  describe "GET 'user'" do
-    it "returns http success" do
-      get 'user'
+  describe "User Seite" do
+    it "HTTP Should not be ok when not logged in" do
+      get 'index'
+      response.should_not be_success
+    end
+    it "HTTP Should not be ok when logged in as user" do
+      user = FactoryGirl.create(:user)
+      sign_in user
+      get 'index'
+      response.should_not be_success
+    end 
+    it "HTTP Should be ok when logged in as admin" do
+      admin = FactoryGirl.create(:admin)
+      sign_in admin
+      get 'index'
       response.should be_success
     end
   end
