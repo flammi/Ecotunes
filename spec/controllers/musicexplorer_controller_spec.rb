4f16381ef6db04921d7ce9f05891574987f6ba95
Fabian Franzen <flammi@watteimdocht.de>
2013-03-02 17:01:09 +0100

    Test für Musicexplorer

--- spec/controllers/musicexplorer_controller_spec.rb
+++ spec/controllers/musicexplorer_controller_spec.rb
@@ -2,11 +2,18 @@ require 'spec_helper'
 
 describe MusicexplorerController do
 
-  describe "Tests der Musikdatenbank" do
-    it "http success" do
+  describe "Tests der Musikdatenbank (Hauptseite)" do
+    it "HTTP Success Return Code wenn eingeloggt" do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      user = FactoryGirl.create :user
+      sign_in user
       get 'explorer'
       response.should be_success
     end
+    it "Failure, wenn nicht eingeloogt" do
+      get 'explorer'
+      response.should_not be_success
+    end 
   end
 
 end
